da0668eb0887c6367c6992040b7a98d72aa30fce
Damar Zaky <damzaky@gmail.com>
2025-04-04 16:49:08 +0900

    fix: working workers

--- wasm-sort/public/worker.js
+++ wasm-sort/public/worker.js
@@ -2,19 +2,39 @@
 self.onmessage = function(e) {
     const { data, sortColumn } = e.data;
 
-    quickSort(data, 0, data.length - 1, sortColumn);
+    quickSortIterative(data, sortColumn);
     self.postMessage(data);
 };
 
-function quickSort(arr, left, right, sortColumn) {
-    if (left < right) {
+function quickSortIterative(arr, sortColumn) {
+    const stack = [];
+    stack.push(0);
+    stack.push(arr.length - 1);
+
+    while (stack.length > 0) {
+        const right = stack.pop();
+        const left = stack.pop();
+
+        if (left >= right) continue;
+
         const pivotIndex = partition(arr, left, right, sortColumn);
-        quickSort(arr, left, pivotIndex - 1, sortColumn);
-        quickSort(arr, pivotIndex + 1, right, sortColumn);
+
+        if (pivotIndex - 1 > left) {
+            stack.push(left);
+            stack.push(pivotIndex - 1);
+        }
+        if (pivotIndex + 1 < right) {
+            stack.push(pivotIndex + 1);
+            stack.push(right);
+        }
     }
 }
 
 function partition(arr, left, right, sortColumn) {
+    // Randomized pivot
+    const pivotIndex = left + Math.floor(Math.random() * (right - left + 1));
+    [arr[pivotIndex], arr[right]] = [arr[right], arr[pivotIndex]];
+
     const pivot = arr[right];
     let i = left - 1;
 
